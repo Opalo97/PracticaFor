c59d1774bc68a6eb41c450b008d6633c399e89e3
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-01-24 10:37:42 +0100

    proyecto numeros primos terminado

---
+++ NumeroPrimo/src/es/lvo/Main.java
@@ -0,0 +1,28 @@
+package es.lvo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero, divisores = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Escribe un número para comprobar si es primo: ");
+        numero = entrada.nextInt();
+
+        for (int i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+                divisores++;
+            }
+        }
+
+        if (divisores == 2) {
+            System.out.println("El número " + numero + " es primo");
+        } else {
+            System.out.println("El número " + numero + " no es primo");
+        }
+    }
+}
+
+
